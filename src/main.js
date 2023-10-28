4133ceba2674e7b1bf85b38f9ba581b7fb1717e6
Walter <waltergerardo67@gmail.com>
2023-10-27 21:46:06 -0600

    Submodulo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
+class Task {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+}
+
+class TaskManager {
+    constructor() {
+        this.tasks = [];
+    }
+
+    addTask(title, description) {
+        const newTask = new Task(title, description);
+        this.tasks.push(newTask);
+        return newTask;
+    }
+
+    deleteTask(task) {
+        const index = this.tasks.indexOf(task);
+        if (index !== -1) {
+            this.tasks.splice(index, 1);
+        }
+    }
+
+    completeTask(task) {
+        task.completed = true;
+    }
+}
+
 const taskForm = document.getElementById("task-form");
 const taskList = document.getElementById("task-list");
+const taskManager = new TaskManager();
 
 taskForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -12,16 +44,52 @@ taskForm.addEventListener("submit", function (e) {
         return;
     }
 
-    const taskItem = document.createElement("li");
-    taskItem.innerHTML = `
-        <h3>${taskTitle}</h3>
-        <p>${taskDescription}</p>
-        <button class="complete-button">Completar</button>
-        <button class="delete-button">Eliminar</button>
-    `;
-
-    taskList.appendChild(taskItem);
+    const newTask = taskManager.addTask(taskTitle, taskDescription);
+    addTaskToDOM(newTask);
 
     // Limpiar el formulario
     taskForm.reset();
 });
+
+function addTaskToDOM(task) {
+    const taskItem = document.createElement("div");
+    taskItem.className = "bg-white rounded-lg shadow-lg p-4";
+    
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "form-checkbox h-5 w-5 text-blue-500 mr-2";
+    checkbox.checked = task.completed;
+    checkbox.addEventListener("change", function () {
+        taskManager.completeTask(task);
+        taskItem.classList.toggle("line-through");
+    });
+
+    const taskTitle = document.createElement("h3");
+    taskTitle.className = "text-xl font-semibold";
+    taskTitle.textContent = task.title;
+    
+    const taskDescription = document.createElement("p");
+    taskDescription.textContent = task.description;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "text-red-500 hover:text-red-700";
+    deleteButton.textContent = "ELIMINAR";
+    deleteButton.addEventListener("click", function () {
+        taskManager.deleteTask(task);
+        taskItem.remove();
+    });
+
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskTitle);
+    taskItem.appendChild(taskDescription);
+    taskItem.appendChild(deleteButton);
+
+    taskList.appendChild(taskItem);
+}
+const toggleDarkMode = () => {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
+const darkModeButton = document.getElementById('dark-mode-button');
+darkModeButton.addEventListener('click', toggleDarkMode);
